a849ac99c34c7733794c8cbea739c0f0a5c8fdec
Prashant Srivastav <77802876+prashant-srivastav-1305@users.noreply.github.com>
2022-01-09 16:07:41 +0530

    Update login_screen.py

--- login_screen.py
+++ login_screen.py
@@ -9,6 +9,9 @@ from tkinter import *
 from tkinter import messagebox
 import backend as bk
 
+font = ('Courier New', 12, 'bold')
+font2 = ('Consolas', 16, 'bold','underline')
+
 
 # Commands
 def login_command(event=None):
@@ -27,52 +30,71 @@ window.title('Alpha Healthcare Login')
 window.configure(bg='teal')
 window.resizable(False, False)
 window.config(padx=10)
+icon = PhotoImage(file='alpha.png')
+window.iconphoto(False, icon)
+
+
+# Image
+img = PhotoImage(file='main_logo.png')
+img_button = Button(window, image=img)
+img_button.grid(row=0,
+                column=0,
+                pady=5)
+
+# Frame to display everything
+mainFrame = Frame(window, bg='teal')
+mainFrame.grid(row=1, column=0)
 
 # Display
-title1 = Label(window, text='--------- |\u03B1|\u2122 ---------', fg='yellow', bg='teal')
-title1.grid(row=0, column=0, columnspan=2)
-
-title2 = Label(window, text='Alpha healthcare\u2122 PORTAL', fg='yellow', bg='teal')
-title2.grid(row=1, column=0, columnspan=2)
+title2 = Label(mainFrame, text='Alpha healthcare\u2122 PORTAL', fg='yellow', bg='teal')
+title2.grid(row=0, column=0, columnspan=2)
+title2.configure(font=font2)
 
 # Entry labels and fields
-l1 = Label(window, text='Username:', fg='white', bg='teal')
-l1.grid(row=2, column=0)
+l1 = Label(mainFrame, text='Username:', fg='white', bg='teal')
+l1.grid(row=1, column=0)
+l1.configure(font= font)
 
-l2 = Label(window, text='Password:', fg='white', bg='teal')
-l2.grid(row=3, column=0)
+l2 = Label(mainFrame, text='Password:', fg='white', bg='teal')
+l2.grid(row=2, column=0)
+l2.configure(font=font)
 
 username = StringVar()
-e1 = Entry(window, textvariable=username)
-e1.grid(row=2, column=1)
+e1 = Entry(mainFrame, textvariable=username)
+e1.grid(row=1, column=1)
+e1.configure(font=font)
 
 password = StringVar()
-e2 = Entry(window, textvariable=password, show='●')
-e2.grid(row=3, column=1)
+e2 = Entry(mainFrame, textvariable=password, show='●')
+e2.grid(row=2,
+        column=1)
 e2.bind('<Return>', login_command)
+e2.configure(font=font)
 
 # Login button with lime background and spanning width of 2 columns
-b1 = Button(window,
+b1 = Button(mainFrame,
             text='Login',
             bg='lime',
             command=login_command,
             activebackground='green',
             activeforeground='white')
-b1.grid(row=4,
+b1.grid(row=3,
         column=0,
         columnspan=2,
         sticky=W + E,
         pady=5)
+b1.configure(font=font)
 
 # Close button to destroy the window
-b2 = Button(window,
+b2 = Button(mainFrame,
             text='Close',
             bg='red',
             command=window.destroy)
-b2.grid(row=5,
+b2.grid(row=4,
         column=0,
         columnspan=2,
         sticky=W + E,
-        pady=5)
+        pady=10)
+b2.configure(font=font)
 
 window.mainloop()
