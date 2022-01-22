158fed1f33d7c1ebe0368b5b02bf955cb6c995c6
Prashant Srivastav <77802876+prashant-srivastav-1305@users.noreply.github.com>
2022-01-22 19:51:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import backend as bk
 
 # FONT TUPLE : TYPE CONSOLAS ,SIZE 11
 font = ('Consolas', 11)
+
+
 def success():
     print('True')
 
@@ -14,7 +16,7 @@ def success():
 # ------------------------------------------COMMANDS---------------------------------------------------------------
 
 
-# ADDING A PATIENT (BUTTON b1 @ LINES 311-318)
+# ADDING A PATIENT (BUTTON b1 @ LINES 376-383)
 # USE FUNCTION bk.addPatient() WITH ALL THE PARAMETERS FROM THE ENTRIES ,RADIOBUTTONS AND DROP-DOWN LISTS
 # SHOW THE OUTPUT IN A TABULAR FORM WITH prettytable AND tkinter.scrolledtext
 # EXCEPTION HANDLING WHEN ALL THE REQUIRED FIELDS ARE NOT FILLED
@@ -35,23 +37,84 @@ def add_patient_command():
         messagebox.showwarning('Data entry error', 'You have not entered all data!')
 
 
-# SEARCHING A PATIENT (BUTTON b2 @ LINES 321-328)
-# USE FUNCTION bk.getInfo() WITH ALL THE PARAMETERS FROM THE ENTRIES, RADIOBUTTONS AND DROP-DOWN LISTS
-# SHOW THE SEARCHED INFORMATION IN A TABULAR FORM WITH prettytable AND tkinter.scrolledtext
-def search_patient_command():
-    searched = bk.getInfo(
-        name.get(), name2.get(), age.get(), sex.get(), ward.get(), doctor.get(), status.get(), days_to_admit.get())
-    table = pt.PrettyTable(['ID', 'F_Name', 'L_Name', 'Age', 'Sex', 'Ward', 'Doctor', 'Status', 'Days'])
-    for row in searched:
-        table.add_row(row)
-    st1.config(state='normal')
-    st1.delete('1.0', 'end')
-    st1.insert(1.0, table)
-    st1.config(state='disabled')
+# SEARCHING A PATIENT (BUTTON b2 @ LINES 386-393)
+# MAKE A NEW WINDOW CALLED sub_window FOR SELECTIVE SEARCHING USING THE tkinter.Toplevel() FUNCTION 
+# USE THAT NEW WINDOW TO SEARCH AND THEN DISPLAY THE SEARCHED DATA IN THE MAIN WINDOW 
+def search_patient_window():
+    sub_window = Toplevel(window)
+
+    # SEARCHING A PATIENT (BUTTON (local) b1 @ LINES 103-111)
+    # USE FUNCTION bk.getInfo() WITH ALL THE PARAMETERS FROM ENTRY AND DROP-DOWN LIST
+    # SHOW THE SEARCHED INFORMATION IN A TABULAR FORM IN THE MASTER WINDOW WITH prettytable AND tkinter.scrolledtext
+    def search_patient_command():
+        ans = bk.getInfo(param.get(), data.get())
+        if len(ans) == 0:
+            messagebox.showinfo('Not found', 'There are no records found with the above data.')
+        else:
+            table = pt.PrettyTable(['ID', 'F_Name', 'L_Name', 'Age', 'Sex', 'Ward', 'Doctor', 'Status', 'Days'])
+            for row in ans:
+                table.add_row(row)
+            st1.config(state='normal')
+            st1.delete('1.0', 'end')
+            st1.insert(1.0, table)
+            st1.config(state='disabled')
+
+    # -------------------------MAIN CODE FOR sub_window----------------------------------------
+    sub_window.title('Searching')
+    sub_window.resizable(False, False)
+
+    # ============================[FRAME]=======================================
+    frame = Frame(sub_window)
+    frame.grid(row=0,
+               column=0,
+               padx=10,
+               pady=5)
+
+    # =====================[LABELS]============================================
+    # SEARCH PARAMETER
+    l1 = Label(frame, text='Parameter to search: ')
+    l1.grid(row=0, column=0)
+    l1.configure(font=font)
+
+    # DATA TO SEARCH 
+    l2 = Label(frame, text='Data to search: ')
+    l2.grid(row=1, column=0, sticky=W)
+    l2.configure(font=font)
+
+    # ========================[ENTRIES]============================================
+    # DATA ENTRY
+    data = StringVar()
+    data.set('Data...')
+    e1 = Entry(frame, textvariable=data)
+    e1.grid(row=1, column=1)
+    e1.configure(font=font)
+
+    # ===========================[DROPDOWN LIST]====================================
+    # PARAMETERS TO SEARCH
+    options = ['Select', 'f_name', 'l_name', 'age', 'sex', 'ward', 'doctor', 'status', 'days']
+    param = StringVar()
+    param.set('Select')
+    dd1 = OptionMenu(frame, param, *options)
+    dd1.grid(row=0, column=1, sticky=W)
+    dd1.configure(font=font)
+
+    # ============================[BUTTONS]==========================================
+    # SEARCH PATIENT BUTTON
+    b1 = Button(frame,
+                text='Search',
+                bg='green',
+                command=search_patient_command)
+    b1.grid(row=2,
+            column=0,
+            columnspan=2,
+            sticky=N + E + W + S)
+    b1.configure(font=font)
+
+    sub_window.mainloop()
 
 
-# UPDATING A PATIENT (BUTTON b3 @ LINES 332-336)
-# TAKE THE ID TO UPDATE USING tkinter.simpledialog AND STORING THE ID IN choice 
+# UPDATING A PATIENT (BUTTON b3 @ LINES 397-401)
+# TAKE THE ID TO UPDATE USING tkinter.simpledialog AND STORING THE ID IN choice
 # USE THE FUNCTION bk.updatePatient_search() TO SEARCH FOR THE REQUIRED ID
 # IF PATIENT IS FOUND THEN ALL THE INFO IS SET INTO THE ENTRIES, WHICH THE USER CAN MODIFY
 # AFTER THE INFO IS SET INTO THE ENTRIES, UPDATE PATIENT BUTTON IS DISABLED AND CONFIRM BUTTON IS ENABLED
@@ -70,16 +133,16 @@ def update_patient_command():
         doctor.set(result[1][0][6])
         status.set(result[1][0][7])
         days_to_admit.set(result[1][0][8])
-        messagebox.showinfo('Data entry','Please change the data to be updated and then press the confirm button!')
+        messagebox.showinfo('Data entry', 'Please change the data to be updated and then press the confirm button!')
         b3.config(state='disabled')
         b1.config(state='disabled')
         b2.config(state='disabled')
         b33.config(state='normal')
     else:
-        messagebox.showwarning('ID not found','The ID you entered is not in the database!')
+        messagebox.showwarning('ID not found', 'The ID you entered is not in the database!')
 
 
-# CONFIRM COMMAND (BUTTON b33 @ LINES 349-351)
+# CONFIRM COMMAND (BUTTON b33 @ LINES 414-416)
 # THIS COMMAND IS USED AFTER THE CONFIRM BUTTON IS ENABLED WHEN THE PATIENT RECORD IS FOUND IN THE DATABASE
 # AND THE ENTRIES ARE SET WITH THE INFO. THE USER NEEDS TO PRESS THIS BUTTON AFTER HE/SHE HAS MODIFIES THE INFORMATION
 # IT USES THE bk.updatePatient() COMMAND WITH ALL THE REQUIRED PARAMETERS TO UPDATE THE PATIENT
@@ -104,9 +167,9 @@ def confirm_command():
     show_all_command()
 
 
-# ADMIT A PATIENT (BUTTON b31 @ LINES 339-340)
+# ADMIT A PATIENT (BUTTON b31 @ LINES 404-405)
 # USE THE FUNCTION bk.admit() WITH ALL THE REQUIRED PARAMETERS TO ADMIT A PATIENT
-# EXCEPTION HANDLING IF ALL THE DATA IS NOT ENTERED 
+# EXCEPTION HANDLING IF ALL THE DATA IS NOT ENTERED
 # APPROPRIATE MESSAGE IS SHOWN USING tkinter.messagebox.showwarning()
 def admit_command():
     try:
@@ -115,18 +178,18 @@ def admit_command():
         )
         show_all_command()
     except:
-        messagebox.showwarning('Data entry error', 'You have not entered all data!')
+        messagebox.showwarning('Data entry error', 'You have not entered all data correctly!')
 
 
-# DISCHARGING A PATIENT (BUTTON b32 @ LINES 344-345)
+# DISCHARGING A PATIENT (BUTTON b32 @ LINES 409-410)
 # WORK REMAINING.....
 def discharge_command():
     return 1.2
 
 
-# SHOWING ALL DATA (BUTTON b4 @ LINES 354-361)
+# SHOWING ALL DATA (BUTTON b4 @ LINES 419-426)
 # SHOW ALL DATA IN THE SCROLLEDTEXT WIDGET USING THE bk.showAll() FUNCTION
-# THE SCROLLEDTEXT WIDGgithubET'S STATE IS DISABLED ONCE THE INFORMATION IS DISPLAYED 
+# THE SCROLLEDTEXT WIDGET'S STATE IS DISABLED ONCE THE INFORMATION IS DISPLAYED
 # SO THAT NO ONE CAN MODIFY IT EXPLICITLY
 def show_all_command():
     table_info = bk.showAll()
@@ -140,17 +203,17 @@ def show_all_command():
         st1.config(state='disabled')
 
 
-# LOGOUT  (BUTTON b5 @ LINES 364-371)
+# LOGOUT  (BUTTON b5 @ LINES 429-436)
 # THE USER IS PROMPTED FOR A CHOICE USING tkinter.messagebox.askyesno()
 # IF THE USER SELECTS YES THE login_screen.py FILE IS RUN AND THE CURRENT WINDOW IS DESTROYED
 # ELSE NOTHING IS DONE AND THE USER REMAINS IN THE CURRENT WINDOW
 def logout_command():
-    choice = messagebox.askyesno('Logout', 'Do you want to logout?')
+    choice = messagebox.askyesno('Logout', 'Do you want to logout and exit?')
     if choice:
         window.destroy()
-        import login_screen
     elif not choice:
         pass
+
 
 # -----------------------------------------------MAIN PROGRAM-----------------------------------------------------------
 
@@ -159,7 +222,7 @@ window.title('Alpha Healthcare Portal')
 window.geometry('750x725')
 window.resizable(False, False)
 
-#================================[LABELS]=======================================
+# ================================[LABELS]=======================================
 
 # NAME
 l1 = Label(window, text='Name:', width=10, anchor=E)
@@ -171,12 +234,12 @@ l2 = Label(window, text='Age:', anchor=E)
 l2.grid(row=1, column=0, sticky=E)
 l2.configure(font=font)
 
-# SEX 
+# SEX
 l3 = Label(window, text='Sex:', anchor=E)
 l3.grid(row=2, column=0, sticky=E)
 l3.configure(font=font)
 
-# WARD 
+# WARD
 l4 = Label(window,
            text='Ward:',
            width=10,
@@ -184,7 +247,7 @@ l4 = Label(window,
 l4.grid(row=3, column=0, sticky=E)
 l4.configure(font=font)
 
-# DOCTOR 
+# DOCTOR
 l5 = Label(window,
            text='Doctor:',
            width=10,
@@ -208,7 +271,7 @@ l7.grid(row=6,
         sticky=E)
 l7.configure(font=font)
 
-#================================[ENTRIES]============================================
+# ================================[ENTRIES]============================================
 
 # THIS FRAME HOLDS THE FIRST AND THE LAST NAME ENTRIES
 name_frame = Frame(window)
@@ -240,6 +303,7 @@ e2.configure(font=font)
 # AGE
 # THE AGE IS STORED IN A StringVar() age
 age = StringVar()
+age.set('')
 e3 = Entry(window, textvariable=age, width=5)
 e3.grid(row=1, column=1, sticky=W, padx=4)
 e3.configure(font=font)
@@ -247,17 +311,17 @@ e3.configure(font=font)
 # DAYS ADMITTED/CONSULTED
 # THE INFORMATION IS STORED IN A StringVar() days_to_admit
 days_to_admit = StringVar()
-days_to_admit.set(1)
+days_to_admit.set('Enter...')
 e4 = Entry(window, textvariable=days_to_admit)
 e4.grid(row=6, column=1, sticky=W)
 e4.configure(font=font)
 
-#===============================[DROPDOWN LISTS]===================================
+# ===============================[DROPDOWN LISTS]===================================
 
 # SEX
-options1 = ['Male', 'Female', 'Other']
+options1 = ['Select', 'Male', 'Female', 'Other']
 sex = StringVar()
-sex.set('Male')
+sex.set('Select')
 dd1 = OptionMenu(window, sex, *options1)
 dd1.grid(row=2, column=1, sticky=W)
 dd1.configure(font=font)
@@ -278,7 +342,7 @@ dd3 = OptionMenu(window, doctor, *options3)
 dd3.grid(row=4, column=1, sticky=W)
 dd3.configure(font=font)
 
-#===================================[RADIO BUTTONS]=======================================
+# ===================================[RADIO BUTTONS]=======================================
 
 # THIS FRAME HOLDS ALL THE RADIO BUTTONS USED TO SELECT THE STATUS
 radio_frame = Frame(window)
@@ -288,6 +352,7 @@ radio_frame.grid(row=5,
 
 # PATIENT STATUS IS STORES IN A StringVar() status
 status = StringVar()
+status.set('')
 # FIRST RADIOBUTTON; STATUS: 'Admit'
 r1 = Radiobutton(radio_frame, text='Admit', variable=status, value='Admit')
 r1.grid(row=0, column=0, sticky=W)
@@ -297,7 +362,7 @@ r2 = Radiobutton(radio_frame, text='Checkup', variable=status, value='Checkup')
 r2.grid(row=0, column=1, sticky=W)
 r2.configure(font=font)
 
-#===================================[BUTTONS]===============================================
+# ===================================[BUTTONS]===============================================
 
 # THIS FRAME HOLDS ALL THE BUTTONS USED FOR THE DIFFERENT OPERATIONS
 op_frame = Frame(window)
@@ -307,7 +372,7 @@ op_frame.grid(row=7,
               padx=60,
               pady=10)
 
-# ADD PATIENT 
+# ADD PATIENT
 b1 = Button(op_frame,
             text='Add patient',
             width=12,
@@ -321,7 +386,7 @@ b1.grid(row=0,
 b2 = Button(op_frame,
             text='Search patient',
             width=12,
-            command=search_patient_command)
+            command=search_patient_window)
 b2.grid(row=0,
         column=1,
         rowspan=2,
@@ -329,11 +394,11 @@ b2.grid(row=0,
 
 # UPDATING A PATIENT
 # ADMIT, DISCHARGE AND CONFIRM ARE BASICALLY EXTENSIONS OF THE UPDATE PATIENT BUTTON
-b3 = Button(op_frame, 
-            text='Update patient', 
-            width=15, 
+b3 = Button(op_frame,
+            text='Update patient',
+            width=15,
             command=update_patient_command)
-b3.grid(row=0, column=2,sticky=W + E)
+b3.grid(row=0, column=2, sticky=W + E)
 
 # ADMITTING A PATIENT
 b31 = Button(op_frame, text='Admit patient', width=15, command=admit_command)
@@ -346,7 +411,7 @@ b32.grid(row=1, column=3)
 
 # CONFIRM BUTTON
 # USED TO CONFIRM THAT THE INFORMATION UPDATED SHOULD BE COMMITTED
-b33 = Button(op_frame, text='Confirm',width=15)
+b33 = Button(op_frame, text='Confirm', width=15)
 b33.grid(row=0, column=3)
 b33.config(state='disabled', command=confirm_command)
 
@@ -374,7 +439,7 @@ b5.grid(row=0,
 # f1 = Frame(window, bg='teal')
 # f1.grid(row=8, column=0)
 
-#===============================[SCROLLEDTEXT WIDGET]===============================================
+# ===============================[SCROLLEDTEXT WIDGET]===============================================
 
 # THIS IS USED TO DISPLAY THE TABLES IN THE APPLICATION
 st1 = scrolledtext.ScrolledText(window, font=('Consolas', '11'), state='disabled', width=90)
@@ -383,4 +448,4 @@ st1.config(background='#f0f0ed')
 
 window.mainloop()
 
-#----------------------------------------------------------END---------------------------------------------------------------
+# ----------------------------------------------------------END---------------------------------------------------------------
